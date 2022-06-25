e1010bc4112c2407f3dc5a9f382146abf91663ac
danahoh <danahoh3@gmail.com>
2022-06-25 15:34:29 +0300

    design nav-bar and menue page

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,9 +1,10 @@
 <template>
+
   <div class="container">
     <h1 class="title">Main Page</h1>
     <RecipePreviewList title="Random Recipes" class="RandomRecipes center" />
     <router-link v-if="!$root.store.username" to="/login" tag="button">You need to Login to vue this</router-link>
-    {{ !$root.store.username }}
+    <!-- {{ !$root.store.username }} -->
     <RecipePreviewList
       title="Last Viewed Recipes"
       :class="{
@@ -31,8 +32,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.title{
+  text-align: center;
+}
 .RandomRecipes {
-  margin: 10px 0 10px;
+  margin: 30px 0 30px;
+  text-align: center;
+
+  
 }
 .blur {
   -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
